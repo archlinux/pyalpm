521b33a958c7edba085c14868afb94ee0226478d
Rémy Oudompheng <remy@archlinux.org>
2011-03-25 00:02:38 +0100

    Add a stub DB class

---
+++ src/db.c
@@ -0,0 +1,83 @@
+/**
+ * db.c : wrapper class around pmdb_t
+ *
+ *  Copyright (c) 2011 Rémy Oudompheng <remy@archlinux.org>
+ *
+ *  This file is part of pyalpm.
+ *
+ *  pyalpm is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  pyalpm is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with pyalpm.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <alpm.h>
+#include <Python.h>
+#include "db.h"
+
+static void pyalpm_db_dealloc(AlpmDB *self);
+
+PyTypeObject AlpmDBType = {
+  PyVarObject_HEAD_INIT(NULL, 0)
+  "alpm.DB",             /*tp_name*/
+  sizeof(AlpmDB),        /*tp_basicsize*/
+  0,                          /*tp_itemsize*/
+  (destructor)pyalpm_db_dealloc,     /*tp_dealloc*/
+  0,                          /*tp_print*/
+  0,                          /*tp_getattr*/
+  0,                          /*tp_setattr*/
+  NULL,                       /*tp_reserved*/
+  0,                          /*tp_repr*/
+  0,                          /*tp_as_number*/
+  0,                          /*tp_as_sequence*/
+  0,                          /*tp_as_mapping*/
+  0,                          /*tp_hash */
+  0,                          /*tp_call*/
+  0,                          /*tp_str*/
+  0,                          /*tp_getattro*/
+  0,                          /*tp_setattro*/
+  0,                          /*tp_as_buffer*/
+  Py_TPFLAGS_DEFAULT,         /*tp_flags*/
+  "libalpm DB object",        /* tp_doc */
+  0,                          /* tp_traverse */
+  0,                          /* tp_clear */
+  0,                          /* tp_richcompare */
+  0,                          /* tp_weaklistoffset */
+  0,                          /* tp_iter */
+  0,                          /* tp_iternext */
+  0,                          /* tp_methods */
+  0,                          /* tp_members */
+  0,                          /* tp_getset */
+  0,                          /* tp_base */
+  0,                          /* tp_dict */
+  0,                          /* tp_descr_get */
+  0,                          /* tp_descr_set */
+  0,                          /* tp_dictoffset */
+  0,                          /* tp_init */
+  0,                          /* tp_alloc */
+  0,                          /* tp_new */
+};
+
+/** Initializes Pb class in module */
+void init_pyalpm_db(PyObject *module) {
+  PyObject *type;
+
+  if (PyType_Ready(&AlpmDBType) < 0)
+    return;
+  type = (PyObject*)&AlpmDBType;
+  Py_INCREF(type);
+  PyModule_AddObject(module, "DB", type);
+}
+
+void pyalpm_db_dealloc(AlpmDB *self) {
+  Py_TYPE(self)->tp_free((PyObject*)self);
+}
