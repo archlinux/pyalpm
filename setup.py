5567640078c4105350c30bd212dde3a02fc94ee7
Rémy Oudompheng <remy@archlinux.org>
2011-03-27 20:25:19 +0200

    Packaging fixes.

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from distutils.core import setup
-from distutils.core import Extension
+# -*- coding: utf-8 -*-
+import os
+import setuptools
 
-alpm = Extension('pyalpm',
-    extra_compile_args = ["-Wall"],
+os.putenv('LC_CTYPE', 'en_US.UTF-8')
+
+alpm = setuptools.Extension('pyalpm',
     libraries = ['alpm'],
     sources = [
         'src/pyalpm.c',
@@ -13,6 +15,10 @@ alpm = Extension('pyalpm',
         'src/optionsobject.c'
         ])
 
-setup(name = 'pyalpm',
+setuptools.setup(name = 'pyalpm',
       version = '0.1',
+      description = 'libalpm bindings for Python 3',
+      author = "Rémy Oudompheng",
+      author_email = "remy@archlinux.org",
+      url = "http://projects.archlinux.org/users/remy/pyalpm.git",
       ext_modules = [alpm])
