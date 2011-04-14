40a1c8f5da8fd81ba3621ff59446c37f54051409
Rémy Oudompheng <remy@archlinux.org>
2011-04-14 20:45:02 +0200

    pycman: nicer progress callback

--- pycman/transaction.py
+++ pycman/transaction.py
@@ -25,6 +25,7 @@ This module defines convenient wrappers around pyalpm functions
 to initialize transactions according to options
 """
 
+import sys
 import traceback
 import pyalpm
 
@@ -35,8 +36,36 @@ def cb_event(*args):
 def cb_conv(*args):
 	print("conversation", args)
 
-def cb_progress(*args):
-	print("progress", args)
+last_target = None
+last_percent = 100
+last_i = -1
+def cb_progress(target, percent, n, i):
+	"Display progress percentage for target i/n"
+	global last_target, last_percent, last_i
+	if len(target) == 0:
+		# abstract progress
+		if percent < last_percent or i < last_i:
+			sys.stdout.write("progress (%d targets)" % n)
+			last_i = 0
+		sys.stdout.write((i - last_i) * '.')
+		sys.stdout.flush()
+		last_i = i
+	else:
+		# progress for some target (write 25 dots for 100%)
+		if target != last_target or percent < last_percent:
+			last_target = target
+			last_percent = 0
+			sys.stdout.write("progress for %s (%d/%d)" % (target, i, n))
+		old_dots = last_percent // 4
+		new_dots = percent // 4
+		sys.stdout.write((new_dots - old_dots) * '.')
+		sys.stdout.flush()
+
+	# final newline
+	if percent == 100 and last_percent < 100:
+		sys.stdout.write('\n')
+		sys.stdout.flush()
+	last_percent = percent
 
 def cb_dl(filename, tx, total):
 	print("download %s: %d/%d" % (filename, tx, total))
