2a3d96911f7d0b1f775b7324c44a974b2429af79
Rémy Oudompheng <remy@archlinux.org>
2011-05-08 10:08:22 +0200

    lsoptdepends: new example script that addresses to help with FS#16108
    Signed-off-by: Rémy Oudompheng <remy@archlinux.org>

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(name = 'pyalpm',
       author_email = "remy@archlinux.org",
       url = "http://projects.archlinux.org/users/remy/pyalpm.git",
       packages = ["pycman"],
-      scripts = ["scripts/pycman"],
+      scripts = ["scripts/pycman", "scripts/lsoptdepends"],
       ext_modules = [alpm])
 
 # vim: set ts=4 sw=4 et tw=0:
