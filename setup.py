1b5abcf6a66fd9f6f6a513dde6bb9383e9bb06a5
Rémy Oudompheng <remy@archlinux.org>
2016-01-31 20:37:14 +0100

    Release version 0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import Extension, setup
 
 os.putenv('LC_CTYPE', 'en_US.UTF-8')
 
-pyalpm_version = '0.7'
+pyalpm_version = '0.8'
 
 cflags = ['-Wall', '-Wextra',
     '-Wconversion',
