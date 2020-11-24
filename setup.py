6c6deb38572f2c3d063fa8da489dc7eaaf4b2782
Eli Schwartz <eschwartz@archlinux.org>
2020-11-23 20:20:29 -0500

    move lots of metadata from setup.py into setup.cfg
    It's declarative and easier to read, and comes with convenient helper
    functions for reading files.

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ os.putenv('LC_CTYPE', 'en_US.UTF-8')
 
 pyalpm_version = '0.9.2'
 
-PYCMAN_SCRIPTS = ['database', 'deptest', 'query', 'remove', 'sync', 'upgrade', 'version']
-
 if __name__ == "__main__":
 
     cflags = ['-Wall', '-Wextra', '-Werror',
@@ -33,28 +31,7 @@ if __name__ == "__main__":
                               'src/pyalpm.h',
                               'src/util.h'])
 
-    with open("README.md", "r") as fh:
-        long_description = fh.read()
-
-    setup(name='pyalpm',
-          version=pyalpm_version,
-          description='libalpm bindings for Python 3',
-          long_description=long_description,
-          long_description_content_type="text/markdown",
-          author="Rémy Oudompheng",
-          author_email="remy@archlinux.org",
-          url="https://projects.archlinux.org/pyalpm.git",
-          packages=["pycman"],
-          scripts=["scripts/lsoptdepends"] + [f'scripts/pycman-{p}' for p in PYCMAN_SCRIPTS],
-          ext_modules=[alpm],
-          classifiers=[
-              'Development Status :: 6 - Mature',
-              'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-              'Programming Language :: C',
-              'Programming Language :: Python :: 3 :: Only',
-              'Topic :: System :: Archiving :: Packaging',
-              'Topic :: System :: Software Distribution',
-              'Topic :: System :: Systems Administration',
-          ])
+    setup(version=pyalpm_version,
+          ext_modules=[alpm])
 
 # vim: set ts=4 sw=4 et tw=0:
