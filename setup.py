58be1196b0625f48be7140dae08ca95db1cdfde1
Eli Schwartz <eschwartz@archlinux.org>
2020-11-23 20:41:19 -0500

    setup.py: only gate the final setup() invocation to __main__
    It is easier to read if you don't need to try to figure out the flow of
    a list of strings, and the non-issueful setuptools.Extension()
    invocation, as they relate to __main__.

--- setup.py
+++ setup.py
@@ -7,30 +7,29 @@ os.putenv('LC_CTYPE', 'en_US.UTF-8')
 
 pyalpm_version = '0.9.2'
 
+cflags = ['-Wall', '-Wextra', '-Werror',
+          '-Wno-unused-parameter', '-Wno-incompatible-pointer-types',
+          '-Wno-cast-function-type', '-std=c99', '-D_FILE_OFFSET_BITS=64']
+
+alpm = Extension('pyalpm',
+                 libraries=['alpm'],
+                 extra_compile_args=cflags + ['-DVERSION="%s"' % pyalpm_version],
+                 language='C',
+                 sources=['src/pyalpm.c',
+                          'src/util.c',
+                          'src/package.c',
+                          'src/db.c',
+                          'src/options.c',
+                          'src/handle.c',
+                          'src/transaction.c'],
+                 depends=['src/handle.h',
+                          'src/db.h',
+                          'src/options.h',
+                          'src/package.h',
+                          'src/pyalpm.h',
+                          'src/util.h'])
+
 if __name__ == "__main__":
-
-    cflags = ['-Wall', '-Wextra', '-Werror',
-              '-Wno-unused-parameter', '-Wno-incompatible-pointer-types',
-              '-Wno-cast-function-type', '-std=c99', '-D_FILE_OFFSET_BITS=64']
-
-    alpm = Extension('pyalpm',
-                     libraries=['alpm'],
-                     extra_compile_args=cflags + ['-DVERSION="%s"' % pyalpm_version],
-                     language='C',
-                     sources=['src/pyalpm.c',
-                              'src/util.c',
-                              'src/package.c',
-                              'src/db.c',
-                              'src/options.c',
-                              'src/handle.c',
-                              'src/transaction.c'],
-                     depends=['src/handle.h',
-                              'src/db.h',
-                              'src/options.h',
-                              'src/package.h',
-                              'src/pyalpm.h',
-                              'src/util.h'])
-
     setup(version=pyalpm_version,
           ext_modules=[alpm])
 
