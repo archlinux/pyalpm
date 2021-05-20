c0001b2f3111e1b4a680e3b99a4d6e51a1faf1eb
Jelle van der Waa <jelle@archlinux.org>
2021-05-20 15:51:36 +0200

    0.10 release
    This release adds compatibility with libalpm v13 and is not backwards
    compatible.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ libalpm = pkgconfig.parse('libalpm')
 
 os.putenv('LC_CTYPE', 'en_US.UTF-8')
 
-pyalpm_version = '0.9.2'
+pyalpm_version = '0.10'
 
 cflags = ['-Wall', '-Wextra', '-Werror',
           '-Wno-unused-parameter', '-Wno-incompatible-pointer-types',
