369700a18b022ddf17e5d2bfa37719eae2823bcb
Jelle van der Waa <jelle@archlinux.org>
2025-05-27 12:30:49 +0200

    Bump to 0.10.12
    Do the walk of shame after this...

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ libalpm = pkgconfig.parse('libalpm')
 
 os.putenv('LC_CTYPE', 'en_US.UTF-8')
 
-pyalpm_version = '0.10.6'
+pyalpm_version = '0.10.12'
 
 cflags = ['-Wall', '-Wextra', '-Werror',
           '-Wno-unused-parameter', '-Wno-incompatible-pointer-types',
